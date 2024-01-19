5ef681adc629af9398865d4d06b13c3717280399
mikeymanoguerra <mikeymanoguerra@gmail.com>
2024-01-19 11:37:37 -0500

    re-init!

---
+++ src/models/project.ts
@@ -0,0 +1,11 @@
+export interface Project {
+  heading: string;
+  body: string;
+  primaryUrl?: string;
+  secondaryUrl?: string;
+  src?: string;
+  featured: boolean;
+  technologies: string;
+  excerpt: string
+}
+
